87a4deb11e4a6930ece5dcb33dd2669022df0e60
colivie000 <casterlyo@gmail.com>
2023-02-24 13:42:56 -0500

    created files

---
+++ source/map.js
@@ -0,0 +1,31 @@
+///////////////////
+// Variables
+///////////////////
+
+
+
+/////////////////////
+// DOM Elements
+/////////////////////
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+// Sizing
+canvas.style.backgroundColor = 'black';
+canvas.height = '70vh';
+canvas.width = '70vw';
+
+/////////////////////
+// Event Listeners
+/////////////////////
+
+
+
+
+/////////////////////
+// Event Handlers
+/////////////////////
+
+
+
+
