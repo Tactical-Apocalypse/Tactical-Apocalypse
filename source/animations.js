018c481d201194bfcca634157f1167fc1ebc3ad8
colivie000 <casterlyo@gmail.com>
2023-02-25 05:41:03 -0500

    created player

---
+++ source/animations.js
@@ -0,0 +1,50 @@
+///////////////
+// Imports
+///////////////
+// import canvas from "./map.js";
+
+///////////////
+// Variables
+///////////////
+let interval, start, now, then, elapsed
+let update
+
+///////////////////////
+// Animation function
+///////////////////////
+
+
+// animationLoop is recursively called with requestAnimationFrame
+function animationLoop () {
+    requestAnimationFrame(animationLoop);
+    now = Date.now();
+    elapsed = now - then;
+    if (elapsed > interval) {
+        then = now - (elapsed % interval);
+        update();
+    };
+};
+
+// The start game function sets animation interval (60 fps) and starts the 'animationLoop' function
+function startAnimation () {
+    interval = 1000 / 60;
+    then = Date.now();
+    start = then;
+    animationLoop();
+};
+
+// The animate function sets or reassigns update and starts the animation
+function animate (newUpdate) {
+    update = newUpdate;
+    startAnimation();
+};
+
+///////////////
+// Exports
+///////////////
+export default animate
+
+
+
+
+
--- source/map.js
+++ source/map.js
@@ -1,19 +1,37 @@
-///////////////////
-// Variables
-///////////////////
+///////////////
+// Imports
+///////////////
+import animate from "./animations.js";
+import Player from "./characters.js"
 
 
-
-/////////////////////
+////////////////
 // DOM Elements
-/////////////////////
+////////////////
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-// Sizing
-canvas.style.backgroundColor = 'black';
-canvas.height = '70vh';
-canvas.width = '70vw';
+
+//////////////
+// Variables
+//////////////
+// let player = new Player ();
+// let width = canvas.offsetLeft + (canvas.offsetWidth / 2);
+// let height = canvas.offsetTop + (canvas.offsetHeight / 2);
+
+
+//////////////
+// Functions
+//////////////
+const player = new Player();
+function update () {
+    player.update();
+    player.create(ctx);
+    console.log('update');
+}
+
+animate(update);
+
 
 /////////////////////
 // Event Listeners
@@ -29,3 +47,10 @@ canvas.width = '70vw';
 
 
 
+///////////////
+// Exports
+///////////////
+export default {
+    canvas,
+    ctx
+}
